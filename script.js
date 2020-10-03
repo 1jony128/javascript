a1caca69a9f079b16923d8b558788e397ecb5a77
Evgeny <1jony128@gmail.com>
2020-10-03 14:54:03 +0900

    Add function

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 
-const number0Films = prompt("Сколько фильмов вы уже смотрели?", '' );
+let number0Films;
+
+function start() {
+    number0Films = +prompt("Сколько фильмов вы уже смотрели?", '' );
+
+    while ( number0Films == "" ||  number0Films == null ||  isNaN(number0Films)){
+        number0Films = +prompt("Сколько фильмов вы уже смотрели?", '' );
+    }
+}
+
+    start();
+
 
 const personalMuvieDB = {
     count: number0Films,
@@ -9,12 +20,44 @@ const personalMuvieDB = {
     privat: false,
 };
 
-const question1 = prompt("Один из последних просмотренных фильмов?", '');
-const question2 = prompt("Насколько оцените его?", '');
-const question3 = prompt("Один из последних просмотренных фильмов?", '');
-const question4 = prompt("Насколько оцените его?", '');
+   
+    function rememberMyfilms () {
+        for (let i=0; i<2; i++){
+            const q1 = prompt("Один из последних просмотренных фильмов?", '');
+            const q2 = prompt("Насколько оцените его?", '');
+    
+            if (q1 != null && q2 != null  && q1 != "" && q2 != "" && q1.length < 50 && q2.length < 50){
+            personalMuvieDB.movies[q1] = q2;
+            console.log("done");
+            } else{
+                console.log("ошибка");
+            }
+        }
+    }
+    
+    rememberMyfilms ();
 
-personalMuvieDB.movies[question1] = question2;
-personalMuvieDB.movies[question3] = question4;
+    if (personalMuvieDB.count <=10) {
+        console.log("Мало");
+    } else if (personalMuvieDB.count >= 10 && personalMuvieDB.count <= 30 ){
+        console.log("Много очень");
+    }
+    let i1  = 0;
+    while(i1<2){
+    const q1 = prompt("Один из последних просмотренных фильмов?", '');
+    const q2 = prompt("Насколько оцените его?", '');
+
+    if (q1 != null && q2 != null  && q1 != "" && q2 != "" && q1.length < 50 && q2.length < 50){
+    personalMuvieDB.movies[q1] = q2;
+    console.log("done");
+    } else{
+        console.log("ошибка");
+        i1--;
+    }
+    i1++;
+}
+
+
+
 
 console.log(personalMuvieDB);
